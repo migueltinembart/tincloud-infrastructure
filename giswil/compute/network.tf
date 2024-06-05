5af96f29297e14ae1991cce1d970b34706ffe9c6
miguel.tinembart <miguel.tinembart@gmail.com>
2024-06-05 17:07:20 +0200

    chore: slimmed network configuration

--- giswil/compute/network.tf
+++ giswil/compute/network.tf
@@ -6,26 +6,14 @@ resource "maas_dns_domain" "tincloud-domain-name" {
 
 }
 
-resource "maas_fabric" "maas_fabrics" {
+resource "maas_fabric" "maas_fabric" {
   name = var.fabric.name
-}
-
-# can be imported
-resource "maas_resource_pool" "maas_pools" {
-  name = var.pool.name
-}
-
-resource "maas_vlan" "maas_vlans" {
-  for_each = var.subnets
-  fabric   = maas_fabric.fabric-1.id
-  name     = each.key
-  vid      = each.value["vlan_id"]
 }
 
 resource "maas_subnet" "tf_subnet" {
   for_each    = var.subnets
   cidr        = each.value["cidr"]
-  fabric      = maas_fabric.fabric-1.id
+  fabric      = maas_fabric.maas_fabric.id
   vlan        = each.value["vlan_id"]
   name        = each.key
   gateway_ip  = each.value["gateway"]
