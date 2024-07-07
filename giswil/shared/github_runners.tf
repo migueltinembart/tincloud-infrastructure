0a05b9fafd8de7d343bfc748d0e9fd9298ee52cc
miguel.tinembart <miguel.tinembart@gmail.com>
2024-07-07 21:55:38 +0200

    delete current instances

--- giswil/shared/github_runners.tf
+++ giswil/shared/github_runners.tf
@@ -3,16 +3,16 @@ data "maas_machine" "kvm_hosts" {
   hostname = each.value
 }
 
-resource "maas_instance" "instances" {
-  for_each = var.pipeline_runners
-  deploy_params {
-    user_data     = file("${path.module}/infra_runner.yaml")
-    distro_series = "jammy"
-  }
-  allocate_params {
-    hostname      = "kvm-1"
-    min_cpu_count = each.value.cores
-    min_memory    = each.value.memory
-  }
-
-}
+## resource "maas_instance" "instances" {
+##   for_each = var.pipeline_runners
+##   deploy_params {
+##     user_data     = file("${path.module}/infra_runner.yaml")
+##     distro_series = "jammy"
+##   }
+##   allocate_params {
+##     hostname      = "kvm-1"
+##     min_cpu_count = each.value.cores
+##     min_memory    = each.value.memory
+##   }
+## 
+## }
