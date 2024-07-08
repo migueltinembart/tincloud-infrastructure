9a8beabf5a2d3d614720c7d0c412e911196118a8
miguel.tinembart <miguel.tinembart@gmail.com>
2024-07-08 23:48:02 +0200

    add directory role

--- az/shared/github.tf
+++ az/shared/github.tf
@@ -21,6 +21,15 @@ resource "azuread_service_principal" "github" {
   owners          = [data.azuread_user.owner.object_id]
 }
 
+resource "azuread_directory_role" "user_administrator" {
+  template_id  = "fe930be7-5e62-47db-91af-98c3a49a38b1"
+}
+
+resource "azuread_directory_role_assignment" "github_role_assignment" {
+  principal_object_id = azuread_service_principal.github.object_id
+  role_id             = azuread_directory_role.user_administrator.id
+}
+
 resource "azurerm_role_assignment" "github" {
   for_each             = data.azurerm_subscription.top-level
   principal_id         = azuread_service_principal.github.object_id
